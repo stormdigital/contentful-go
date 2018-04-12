50c92eae61eaae2a7b7156079df562bc28d367b8
ddevcap <daan@devcap.nl>
2018-04-12 15:54:52 +0200

    added topic constants

---
+++ topics.go
@@ -0,0 +1,25 @@
+package contentful
+
+const (
+	TopicContentTypeCreate    = "ContentManagement.ContentType.create"
+	TopicContentTypeSave      = "ContentManagement.ContentType.save"
+	TopicContentTypePublish   = "ContentManagement.ContentType.publish"
+	TopicContentTypeUnpublish = "ContentManagement.ContentType.unpublish"
+	TopicContentTypeDelete    = "ContentManagement.ContentType.delete"
+	TopicEntryCreate          = "ContentManagement.Entry.create"
+	TopicEntrySave            = "ContentManagement.Entry.save"
+	TopicEntryAutoSave        = "ContentManagement.Entry.auto_save"
+	TopicEntryArchive         = "ContentManagement.Entry.archive"
+	TopicEntryUnarchive       = "ContentManagement.Entry.unarchive"
+	TopicEntryPublish         = "ContentManagement.Entry.publish"
+	TopicEntryUnpublish       = "ContentManagement.Entry.unpublish"
+	TopicEntryDelete          = "ContentManagement.Entry.delete"
+	TopicAssetCreate          = "ContentManagement.Asset.create"
+	TopicAssetSave            = "ContentManagement.Asset.save"
+	TopicAssetAutoSave        = "ContentManagement.Asset.auto_save"
+	TopicAssetArchive         = "ContentManagement.Asset.archive"
+	TopicAssetUnarchive       = "ContentManagement.Asset.unarchive"
+	TopicAssetPublish         = "ContentManagement.Asset.publish"
+	TopicAssetUnpublish       = "ContentManagement.Asset.unpublish"
+	TopicAssetDelete          = "ContentManagement.Asset.delete"
+)
